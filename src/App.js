39b07a2148ea997d406307030342d4fd49061ac8
Shannon King <kingshannon01@gmail.com>
2019-04-23 09:13:55 -0700

    score does not update; added pseudocode logic

--- src/App.js
+++ src/App.js
@@ -61,21 +61,39 @@ class App extends React.Component {
     clickedImages: []
   };
 
-  handleClickedImages = () => {
-    console.log(App.imgClick.imgId);
-    // this.setState({ clickedImages: App.imgClick.imgId });
+  recordClickedImages = () => {
+    console.log(images.id);
+    // When img is clicked, record value of 'id' (from image objects in const images array) into clickedImages[].
+    // update the state of clickedImages[] 
+  
+    // this.setState({ clickedImages: this.state.clickedImages + images.id });
+  };
+
+  //onClick do this: Compare id of new click to all ids recorded in clickedImages[]:
+  
+  //if id of newCLick === any id in clickedImages[] 
+  //then Score resets to 0. 
+  // High score is reset. 
+  // Game re-starts. 
+  // clickedImages[] state is reset to empty array.
+  // Images shuffle randomly. 
+
+
+  //if id of newCLick !=== any id in clickedImages[] 
+  //then Score increments by 1. 
+  // id is added to clickedImages[]. 
+  // Images shuffle randomly. 
+
+
+  imgClick = (id) => {
+    console.log(id);
   };
 
   handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
   };
 
-  imgClick = (id) => {
-    console.log(id);
-  };
-
   render() {
-
     return (
       <div>
         <ul className="nav">
@@ -95,10 +113,10 @@ class App extends React.Component {
         <h3>Click on an image to earn points, but don't click any more than once!</h3>
 
         <div className="card">
-          <div className="img-container" onClick={this.handleIncrement.imgClick} onClick={this.handleClickedImages.imgClick}>
+          <div className="img-container" onClick={this.handleIncrement.imgClick} onClick={this.recordClickedImages.imgClick} >
 
             {images.map(image => <Image image={image.imgURL} key={image.id} imgClick={this.imgClick} imgId={image.id} />)}
-          
+
           </div>
         </div>
       </div>
