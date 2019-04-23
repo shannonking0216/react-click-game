89c4cd34d95464dce9049cfaba5121d06761f21c
Shannon King <kingshannon01@gmail.com>
2019-04-22 22:58:21 -0700

    img with id: 1 is the only one showing up; css card style working

--- src/App.js
+++ src/App.js
@@ -4,53 +4,53 @@ import Image from './Image.js';
 
 const images =
   [
-    { 
-      id: 1, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/goldenford.png')" 
+    {
+      id: 1,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/goldenford.png')"
     },
-    { 
-      id: 2, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/birdperson.png')" 
+    {
+      id: 2,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/birdperson.png')"
     },
-    { 
-      id: 3, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/jerry.png')" 
+    {
+      id: 3,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/jerry.png')"
     },
-    { 
-      id: 4, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/beth.png')" 
+    {
+      id: 4,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/beth.png')"
     },
-    { 
-      id: 5, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/gianthead.png')" 
+    {
+      id: 5,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/gianthead.png')"
     },
-    { 
-      id: 6, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/summer.png')" 
+    {
+      id: 6,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/summer.png')"
     },
-    { 
-      id: 7, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/meeseeks.png')" 
+    {
+      id: 7,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/meeseeks.png')"
     },
-    { 
-      id: 8, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/mr.png')" 
+    {
+      id: 8,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/mr.png')"
     },
-    { 
-      id: 9, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/evilmorty.png')" 
+    {
+      id: 9,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/evilmorty.png')"
     },
-    { 
-      id: 10, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/rick.png')" 
+    {
+      id: 10,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/rick.png')"
     },
-    { 
-      id: 11, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/jessica.png')" 
+    {
+      id: 11,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/jessica.png')"
     },
-    { 
-      id: 12, 
-      imgURL: "url('https://clicky-game.netlify.com/assets/images/morty.png')" 
+    {
+      id: 12,
+      imgURL: "url('https://clicky-game.netlify.com/assets/images/morty.png')"
     },
   ];
 
@@ -74,19 +74,23 @@ class App extends React.Component {
 
     return (
       <div>
+
         <h1>RICK and MORTY MEMORY GAME</h1>
         <h2>Click on an image to earn points, but don't click any more than once!</h2>
-        {images.map(image => <Image image={image.imgURL} key={image.id} imgClick={this.imgClick} imgId={image.id} />)}
-        <div className="card text-center">
-          <div className="card-header bg-primary text-white">
-            Click Counter!
-        </div>
+
+
+        <div className="card">
+          <div className="img-container">
+              {images.map(image => <Image image={image.imgURL} key={image.id} imgClick={this.imgClick} imgId={image.id} />)}
+          </div>
+
           <div className="card-body">
-            <p className="card-text">Click Count: {this.state.count}</p>
+            <p className="card-text">Score: {this.state.count}</p>
             <button className="btn btn-primary" onClick={this.handleIncrement}>
               Increment
           </button>{" "}
           </div>
+
         </div>
       </div>
     );
